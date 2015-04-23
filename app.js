c46fc27c5cac10672de141d9e3cf8ea1786c3308
grace <mmoussasow@gmail.com>
2015-04-24 01:00:17 +0200

    test de la cartographie

--- app.js
+++ app.js
@@ -5,9 +5,10 @@
 		$("body").toggleClass('with--sidebar');
 	});
 
-	$("site-cache").click(function(e){
-		$('body').removeClass('with--sidebar');
-	})
+	$("#site-cache").click(function(e){
+		e.preventDefault();
+		$('body').toggleClass('with--sidebar');
+	});
 
 
 })(jQuery);
