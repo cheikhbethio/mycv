4a314499a866e35fc4df4511642a92caf448839e
cheikhbehio <mmoussasow@gmail.com>
2015-04-24 16:32:24 +0200

    cartographie ok et formulaire en cours

---
+++ test/app.js
@@ -0,0 +1,15 @@
+function initialisation(){
+				var centreCarte = new google.maps.LatLng(44.826971, -0.559583);
+				var optionsCarte = {
+					zoom: 6,
+					center: centreCarte
+				}
+				var maCarte = new google.maps.Map(document.getElementById("EmplacementDeMaCarte"), optionsCarte);
+			
+				//création du marqueur
+		        var marqueur = new google.maps.Marker({
+		          position: centreCarte,
+		          map: maCarte
+		        });
+	    	};
+			google.maps.event.addDomListener(window, 'load', initialisation)
